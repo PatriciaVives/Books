79e955230438b41c581a66a9b472b41265fcca52
PatriciaVives <patricia.debels@student.vives.be>
2024-12-30 17:46:26 +0100

    update database.py file

--- database.py
+++ database.py
@@ -35,13 +35,19 @@ def add_book(title, author, purchase_date, status, in_collection):
 	conn = connect_to_db()
 	cursor = conn.cursor()
 
-	cursor.execute('''INSERT INTO books (title, author, purchase_date, status, in_collection)
-						VALUES (?, ?, ?, ?, ?)''', (title, author, purchase_date, status, in_collection))
+	cursor.execute('SELECT * FROM books WHERE title = ?', (title,))
+	existing_book = cursor.fetchall()
+	if existing_book:
+		print(f"Book '{title}' is already in the list!")
+	else:
+		cursor.execute('''INSERT INTO books (title, author, purchase_date, status, in_collection)
+							VALUES (?, ?, ?, ?, ?)''', (title, author, purchase_date, status, in_collection))
 
-	conn.commit()
+		conn.commit()
+		print(f"Book '{title}' is added to the list!")
+
 	conn.close()
-	print(f"Book '{title}' is added!")
-
+	
 #functie opvragen boeken
 def get_books():
 	conn = connect_to_db()
@@ -58,9 +64,24 @@ def add_review(book_id, rating, finished_reading_dated, comments):
 	conn = connect_to_db()
 	cursor = conn.cursor()
 
-	cursor.execute('''INSERT INTO reviews (book_id, rating, finished_reading_dated, comments)
-						VALUES (?, ?, ?, ?, ?)''', (book_id, rating, finished_reading_dated, comments))
+	cursor.execute('SELECT * FROM books WHERE book_id = ?', (book_id))
+	book = cursor.fetchone()
+	if not book:
+		print(f"Book with ID {book_id} does not exist!")
+		conn.close()
+		return
 
-	conn.commit()
+	cursor.execute('SELECT * FROM reviews WHERE book_id = ?', (book_id))
+	existing_review = cursor.fetchone()
+	if existing_review:
+		print(f"Book with ID {book_id} has already been reviewed")
+	elif rating < 1 or > 5:
+		print("Rating must be between 1 and 5!")
+	else:
+		cursor.execute('''INSERT INTO reviews (book_id, rating, finished_reading_dated, comments)
+							VALUES (?, ?, ?, ?, ?)''', (book_id, rating, finished_reading_dated, comments))
+
+		conn.commit()
+		print(f"Review for book with id '{book_id}' is added!")
+
 	conn.close()
-	print(f"Review for book with id '{book_id}' is added!")
